dd9281b068dd528360c1be568a5611b2a4b660d3
Tonny-Kioko <anthonykioko5525@gmail.com>
2022-11-13 12:21:28 +0300

    Setting up PlayerOne

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -5,7 +5,7 @@ class AppConstants{
   static const String BASE_URL = "https//playerone.com";
 
   static const String POPULAR_GAME_URI = "/api/v1/games/popular";
-  static const String RECOMMENDED_GAME_URI = "/api/v1/games/recommendedforgames";
+  static const String RECOMMENDED_GAME_URI = "/api/v1/games/recommended";
 
   static const String TOKEN = "PlayerOne";
 
