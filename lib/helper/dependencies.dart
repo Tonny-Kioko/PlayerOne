1a412425dc6b2629e9c3bbc14f40b2d5106fdecc
Tonny-Kioko <anthonykioko5525@gmail.com>
2022-11-13 11:54:26 +0300

    making changes

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -2,17 +2,23 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:playerone/data/api/api_client.dart';
 import 'package:playerone/data/controllers/popular_games-controller.dart';
+import 'package:playerone/data/controllers/recommended_games_controller.dart';
 import 'package:playerone/data/repository/popular_games_repository.dart';
+import 'package:playerone/data/repository/recommended_games_repository.dart';
 import 'package:playerone/utils/app_constants.dart';
 
 //Dependencies for the API clients
 Future<void> init() async {
-  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.POPULAR_GAME_URI));
+  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
 
 //Dependency for the repositories
   Get.lazyPut(() => PopularGamesRepo(apiClient: Get.find()));
+  Get.lazyPut(() => RecommendedGamesRepo(apiClient: Get.find()));
 
 //Dependencies for controllers
   Get.lazyPut(() =>
       PopularGamesController(popularGamesRepo: Get.find(), apiClient: null));
+  Get.lazyPut(() =>
+      RecommendedGamesController(recommendedGamesRepo: Get.find(), apiClient: null));
+
 }
--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,11 +1,11 @@
 class AppConstants{
   static const String APP_NAME = "PlayerOne";
-  static const double APP_VERSION = 1.0;
+  static const double APP_VERSION = 1;
 
   static const String BASE_URL = "https//playerone.com";
 
   static const String POPULAR_GAME_URI = "/api/v1/games/popular";
   static const String RECOMMENDED_GAME_URI = "/api/v1/games/recommended";
 
-  static const String TOKEN = "readyPlayerOne";
+  static const String TOKEN = "PlayerOne";
 }
