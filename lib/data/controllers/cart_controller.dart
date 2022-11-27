f65c96283467f194787a767f387ab69c69ec1d4b
Tonny-Kioko <anthonykioko5525@gmail.com>
2022-11-27 12:40:35 +0300

    fixing game model

--- lib/data/controllers/cart_controller.dart
+++ lib/data/controllers/cart_controller.dart
@@ -1,9 +1,17 @@
 import 'package:get/get.dart';
 import 'package:playerone/data/repository/cart_controller.dart';
 
-class CartController extends GetxController{
+import '../../models/cart_model.dart';
+import '../../models/games_model.dart';
+
+class CartController extends GetxController {
   final CartRepo cartRepo;
+
   CartController({required this.cartRepo});
 
-  Map<int CartModel>
+  Map<int, CartModel> _items = {};
+
+  void addItem(GameModel game, int quantity) {
+    _items.putIfAbsent(game.id!, () => null)
+  }
 }
