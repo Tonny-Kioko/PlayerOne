0a7fc9dda2f49107e163911327ab3c42d67ee9a5
Tonny-Kioko <anthonykioko5525@gmail.com>
2022-11-27 12:07:40 +0300

    setting up controllers

---
+++ lib/data/controllers/cart_controller.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+import 'package:playerone/data/repository/cart_controller.dart';
+
+class CartController extends GetxController{
+  final CartRepo cartRepo;
+  CartController({required this.cartRepo});
+}
--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:playerone/data/api/api_client.dart';
+import 'package:playerone/data/controllers/cart_controller.dart';
 import 'package:playerone/data/controllers/popular_games-controller.dart';
 import 'package:playerone/data/controllers/recommended_games_controller.dart';
+import 'package:playerone/data/repository/cart_controller.dart';
 import 'package:playerone/data/repository/popular_games_repository.dart';
 import 'package:playerone/data/repository/recommended_games_repository.dart';
 import 'package:playerone/utils/app_constants.dart';
@@ -14,11 +16,14 @@ Future<void> init() async {
 //Dependency for the repositories
   Get.lazyPut(() => PopularGamesRepo(apiClient: Get.find()));
   Get.lazyPut(() => RecommendedGamesRepo(apiClient: Get.find()));
+  Get.lazyPut(() => CartRepo());
 
 //Dependencies for controllers
   Get.lazyPut(() =>
       PopularGamesController(popularGamesRepo: Get.find(), apiClient: null));
   Get.lazyPut(() =>
       RecommendedGamesController(recommendedGamesRepo: Get.find(), apiClient: null));
+  Get.lazyPut(() =>
+      CartController(cartRepo : Get.find()));
 
 }
