0c94f1f4375a129365e53897d9a09c2f04ae88e1
Tonny-Kioko <anthonykioko5525@gmail.com>
2022-12-03 12:08:27 +0300

    updating the basket with new item  ids

--- lib/data/controllers/cart_controller.dart
+++ lib/data/controllers/cart_controller.dart
@@ -12,14 +12,30 @@ class CartController extends GetxController {
   Map<int, CartModel> _items = {};
 
   void addItem(GameModel game, int quantity) {
-    _items.putIfAbsent(game.id!, () => CartModel(
-      quantity: quantity,
-      isExist: true,
-      time: DateTime.now().toString(),
-      name: game.name,
-      id: game.id,
-      image: game.image,
-      price: game.price,
-    ));
+    if (_items.containsKey(game.id!)) {
+      _items.update(game.id!, (value) {
+        return CartModel(
+          quantity: quantity,
+          isExist: true,
+          time: DateTime.now().toString(),
+          name: game.name,
+          id: game.id,
+          image: game.image,
+          price: game.price,
+        );
+      });
+
+    } else {
+      _items.putIfAbsent(game.id!, () =>
+          CartModel(
+            quantity: quantity,
+            isExist: true,
+            time: DateTime.now().toString(),
+            name: game.name,
+            id: game.id,
+            image: game.image,
+            price: game.price,
+          ));
+    }
   }
 }
