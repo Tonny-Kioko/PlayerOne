edf609b5caf081fc8bcfba5637ebbad1216121bf
Tonny-Kioko <cfakiokoanthony@gmail.com>
2022-12-09 13:16:27 +0300

    creating rotes for the cart

--- lib/routes/route_helper.dart
+++ lib/routes/route_helper.dart
@@ -4,17 +4,20 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:playerone/pages/game/popular_game_details.dart';
 import 'package:playerone/pages/home/main_game_page.dart';
 
+import '../pages/cart/cart_page.dart';
 import '../pages/game/recommended_games_details.dart';
 
 class RouteHelper{
 static const String initial = "/";
 static const String popularGame = "/popular-game";
 static const String recommendedGame = "/recommended-game";
+static const String cartPage = "/cart-page";
 
 
 static String getinitial() => '$initial';
 static String getPopularGame(int pageId) => '$popularGame? pageId = $pageId';
- static String getrecommendedGame(int pageId) => '$recommendedGame? pageId = $pageId';
+static String getrecommendedGame(int pageId) => '$recommendedGame? pageId = $pageId';
+static String getCartPage() => '$cartPage';
 
 
 static List<GetPage> routes = [
@@ -26,6 +29,11 @@ static List<GetPage> routes = [
 },
 transition: Transition.fadeIn
 ),
+  GetPage(name: cartPage, page: (){
+    return CartPage();
+  },
+    transition: Transition.fadeIn
+  ),
 GetPage(name: recommendedGame, page:() {
   var pageId = Get.parameters['pageId'];
 return RecommendedGameDetails(pageId: int.parse(pageId!))
