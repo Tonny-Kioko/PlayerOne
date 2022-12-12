3e73fee64bc3ec10d4bd2a1c032664a8f8ae1d20
Tonny-Kioko <cfakiokoanthony@gmail.com>
2022-12-12 12:28:04 +0300

    making images on cart clickable

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:playerone/colors.dart';
 import 'package:playerone/data/controllers/cart_controller.dart';
+import 'package:playerone/data/controllers/popular_games-controller.dart';
+import 'package:playerone/pages/game/popular_game_details.dart';
 import 'package:playerone/pages/home/main_game_page.dart';
 import 'package:playerone/utils/app_constants.dart';
 import 'package:playerone/utils/dimensions.dart';
@@ -9,6 +11,7 @@ import 'package:playerone/widgets/app_icon.dart';
 import 'package:playerone/widgets/big_text.dart';
 import 'package:playerone/widgets/small_text.dart';
 
+import '../../data/controllers/recommended_games_controller.dart';
 import '../../routes/route_helper.dart';
 
 class CartPage extends StatelessWidget {
@@ -20,35 +23,39 @@ class CartPage extends StatelessWidget {
       body: Stack(
         children: [
           Positioned(
-              left: Dimensions.sizeBoxWidth5*3,
+              left: Dimensions.sizeBoxWidth5 * 3,
               right: Dimensions.sizeBoxWidth20,
               top: Dimensions.sizeBoxHeight50,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  AppIcon(icon: Icons.arrow_back_ios_new_outlined,
-                  iconColor: Colors.white,
-                  backgroundColor: AppColors.mainColor,
-                  size: Dimensions.iconSize15*3),
-                  SizedBox(width: Dimensions.sizeBoxWidth20*5,),
-                  GestureDetector(
-                    onTap: (){
-                      Get.toNamed(RouteHelper.getinitial());
-                    },
-                    child: AppIcon(icon: Icons.home_outlined,
-                        iconColor: Colors.white,
-                        backgroundColor: AppColors.mainColor,
-                        size: Dimensions.iconSize15*3),
-                  ),
-                  AppIcon(icon: Icons.shopping_basket_outlined,
+                  AppIcon(
+                      icon: Icons.arrow_back_ios_new_outlined,
                       iconColor: Colors.white,
                       backgroundColor: AppColors.mainColor,
-                      size: Dimensions.iconSize15*3)
+                      size: Dimensions.iconSize15 * 3),
+                  SizedBox(
+                    width: Dimensions.sizeBoxWidth20 * 5,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Get.toNamed(RouteHelper.getinitial());
+                    },
+                    child: AppIcon(
+                        icon: Icons.home_outlined,
+                        iconColor: Colors.white,
+                        backgroundColor: AppColors.mainColor,
+                        size: Dimensions.iconSize15 * 3),
+                  ),
+                  AppIcon(
+                      icon: Icons.shopping_basket_outlined,
+                      iconColor: Colors.white,
+                      backgroundColor: AppColors.mainColor,
+                      size: Dimensions.iconSize15 * 3)
                 ],
-
-          )),
+              )),
           Positioned(
-              top: Dimensions.sizeBoxHeight20*5,
+              top: Dimensions.sizeBoxHeight20 * 5,
               left: Dimensions.sizeBoxWidth20,
               right: Dimensions.sizeBoxWidth20,
               bottom: 0,
@@ -58,100 +65,168 @@ class CartPage extends StatelessWidget {
                 child: MediaQuery.removePadding(
                   context: context,
                   removeTop: true,
-                  child: GetBuilder<CartController>(builder: (cartController){
-                    var _cartList = cartController.getItems;
-                    return ListView.builder(
-                        itemCount: _cartList.length,
-                        itemBuilder: (_, index){
-                          return Container(
-                            height: 100,
-                            width: double.maxFinite,
-                            child: Row(
-                              children: [
-                                Container(
-                                  width: Dimensions.sizeBoxHeight20*5,
-                                  height: Dimensions.sizeBoxHeight20*5,
-                                  margin: EdgeInsets.only(bottom: Dimensions.sizeBoxHeight10),
-                                  decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                        fit: BoxFit.cover,
-                                        image: NetworkImage(
-                                          AppConstants.BASE_URL+AppConstants.UPLOAD_URL+cartController.getItems[index].image!
-                                        ),
-                                    ),
-                                    borderRadius: BorderRadius.circular(Dimensions.radius15),
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                SizedBox(width: Dimensions.sizeBoxWidth10,),
-                                Expanded(child: Container(
-                                  height: Dimensions.sizeBoxHeight20*5,
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      BigText(text: cartController.getItems[index].name!, color: Colors.black54,),
-                                      SmallText(text:cartController.getItems[index].genre!, size: Dimensions.font20,),
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          SmallText(text: cartController.getItems[index]!.price.toString(), color: Colors.black54,),
-                                          Container(
-                                            padding: EdgeInsets.only(
-                                                top: Dimensions.sizeBoxHeight10,
-                                                bottom: Dimensions.sizeBoxHeight10,
-                                                left: Dimensions.sizeBoxWidth10,
-                                                right: Dimensions.sizeBoxWidth10),
-                                            decoration: BoxDecoration(
-                                                borderRadius: BorderRadius.circular(Dimensions.radius20),
-                                                color: Colors.white),
-                                            child: Row(
-                                              children: [
-                                                GestureDetector(
-                                                  onTap: () {
-                                                    //popularGames.setQuantity(false);
-                                                    cartController.addItem(_cartList[index].game!, -1);
-                                                    print("I'm working just fine, pal!");
-                                                  },
-                                                  child: Icon(
-                                                    Icons.remove,
-                                                    color: AppColors.signColor,
-                                                  ),
-                                                ),
-                                                SizedBox(
-                                                  width: Dimensions.sizeBoxHeight10 / 2,
-                                                ),
-                                                SmallText(text: _cartList[index].quantity!.toString()),//popularGames.inCartItems.toString()),
-                                                SizedBox(
-                                                  width: Dimensions.sizeBoxHeight10 / 2,
-                                                ),
-                                                GestureDetector(
-                                                  onTap: () {
-                                                    //popularGames.setQuantity(true);
-                                                    cartController.addItem(_cartList[index].game!, 1);
-                                                    print("I'm working just fine, pal!");
+                  child: GetBuilder<CartController>(
+                    builder: (cartController) {
+                      var _cartList = cartController.getItems;
+                      return ListView.builder(
+                          itemCount: _cartList.length,
+                          itemBuilder: (_, index) {
+                            return Container(
+                              height: 100,
+                              width: double.maxFinite,
+                              child: Row(
+                                children: [
+                                  GestureDetector(
+                                    onTap: () {
+                                      var popularIndex =
+                                          Get.find<PopularGamesController>()
+                                              .popularGamesList
+                                              .indexOf(_cartList[index].game!);
 
-                                                  },
-                                                  child: Icon(
-                                                    Icons.add,
-                                                    color: AppColors.signColor,
-                                                  ),
-                                                )
-                                              ],
-                                            ),
-                                          )
-                                        ],
-                                      )
-                                    ],
+                                      if (popularIndex >= 0) {
+                                        Get.toNamed(RouteHelper.getPopularGame(
+                                            popularIndex));
+                                      } else {
+                                        var recommendedIndex = Get.find<
+                                                RecommendedGamesController>()
+                                            .recommendedGamesList
+                                            .indexOf(_cartList[index].game!);
+                                        Get.toNamed(
+                                            RouteHelper.getrecommendedGame(
+                                                recommendedIndex));
+                                      }
+                                    },
+                                    child: Container(
+                                      width: Dimensions.sizeBoxHeight20 * 5,
+                                      height: Dimensions.sizeBoxHeight20 * 5,
+                                      margin: EdgeInsets.only(
+                                          bottom: Dimensions.sizeBoxHeight10),
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(
+                                              AppConstants.BASE_URL +
+                                                  AppConstants.UPLOAD_URL +
+                                                  cartController
+                                                      .getItems[index].image!),
+                                        ),
+                                        borderRadius: BorderRadius.circular(
+                                            Dimensions.radius15),
+                                        color: Colors.white,
+                                      ),
+                                    ),
                                   ),
-                                ))
-                              ],
-                            ),
-                          );
-                        });
-                  },),
+                                  SizedBox(
+                                    width: Dimensions.sizeBoxWidth10,
+                                  ),
+                                  Expanded(
+                                      child: Container(
+                                    height: Dimensions.sizeBoxHeight20 * 5,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        BigText(
+                                          text: cartController
+                                              .getItems[index].name!,
+                                          color: Colors.black54,
+                                        ),
+                                        SmallText(
+                                          text: cartController
+                                              .getItems[index].genre!,
+                                          size: Dimensions.font20,
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            SmallText(
+                                              text: cartController
+                                                  .getItems[index]!.price
+                                                  .toString(),
+                                              color: Colors.black54,
+                                            ),
+                                            Container(
+                                              padding: EdgeInsets.only(
+                                                  top: Dimensions
+                                                      .sizeBoxHeight10,
+                                                  bottom: Dimensions
+                                                      .sizeBoxHeight10,
+                                                  left:
+                                                      Dimensions.sizeBoxWidth10,
+                                                  right: Dimensions
+                                                      .sizeBoxWidth10),
+                                              decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          Dimensions.radius20),
+                                                  color: Colors.white),
+                                              child: Row(
+                                                children: [
+                                                  GestureDetector(
+                                                    onTap: () {
+                                                      //popularGames.setQuantity(false);
+                                                      cartController.addItem(
+                                                          _cartList[index]
+                                                              .game!,
+                                                          -1);
+                                                      print(
+                                                          "I'm working just fine, pal!");
+                                                    },
+                                                    child: Icon(
+                                                      Icons.remove,
+                                                      color:
+                                                          AppColors.signColor,
+                                                    ),
+                                                  ),
+                                                  SizedBox(
+                                                    width: Dimensions
+                                                            .sizeBoxHeight10 /
+                                                        2,
+                                                  ),
+                                                  SmallText(
+                                                      text: _cartList[index]
+                                                          .quantity!
+                                                          .toString()), //popularGames.inCartItems.toString()),
+                                                  SizedBox(
+                                                    width: Dimensions
+                                                            .sizeBoxHeight10 /
+                                                        2,
+                                                  ),
+                                                  GestureDetector(
+                                                    onTap: () {
+                                                      //popularGames.setQuantity(true);
+                                                      cartController.addItem(
+                                                          _cartList[index]
+                                                              .game!,
+                                                          1);
+                                                      print(
+                                                          "I'm working just fine, pal!");
+                                                    },
+                                                    child: Icon(
+                                                      Icons.add,
+                                                      color:
+                                                          AppColors.signColor,
+                                                    ),
+                                                  )
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ))
+                                ],
+                              ),
+                            );
+                          });
+                    },
+                  ),
                 ),
-          ))
+              ))
         ],
       ),
     );
