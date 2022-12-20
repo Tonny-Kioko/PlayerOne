055f07e4a75f43b88690f9a2b4a328a2184b0978
Tonny-Kioko <cfakiokoanthony@gmail.com>
2022-12-20 13:50:38 +0300

    time in the history basket

--- lib/pages/cart/cart_history.dart
+++ lib/pages/cart/cart_history.dart
@@ -45,6 +45,27 @@ class CartHistory extends StatelessWidget {
 
     List<int> itemsPerOrder = cartItemsPerOrderToList();
     var listCounter = 0;
+    Widget timeWidget (int index){
+      var outputDate = DateTime.now().toString();
+      if(index< getCartHistoryList.length) {
+        DateTime parseDate = DateFormat(
+            "yyyy-MM-dd HH:mm:ss")
+            .parse(
+            getCartHistoryList[listCounter]
+                .time!);
+        var inputDate = DateTime.parse(
+            parseDate.toString());
+        var outputFormat =
+        DateFormat("dd/MM/yyyy hh:mm");
+        var outputDate =
+        outputFormat.format(inputDate);
+        return BigText(
+          text: outputDate,
+        );
+      }
+
+      return BigText(text: outputDate);
+    }
 
     return Scaffold(
       body: Column(
@@ -89,22 +110,7 @@ class CartHistory extends StatelessWidget {
                                     crossAxisAlignment:
                                         CrossAxisAlignment.start,
                                     children: [
-                                      (() {
-                                        DateTime parseDate = DateFormat(
-                                                "yyyy-MM-dd HH:mm:ss")
-                                            .parse(
-                                                getCartHistoryList[listCounter]
-                                                    .time!);
-                                        var inputDate = DateTime.parse(
-                                            parseDate.toString());
-                                        var outputFormat =
-                                            DateFormat("dd/MM/yyyy hh:mm");
-                                        var outputDate =
-                                            outputFormat.format(inputDate);
-                                        return BigText(
-                                          text: outputDate,
-                                        );
-                                      }()),
+                                      timeWidget(listCounter),
                                       SizedBox(
                                         height: Dimensions.sizeBoxHeight10,
                                       ),
