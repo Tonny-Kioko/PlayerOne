969a342c24599685380dea7e2eada47059608e39
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-01 13:06:56 +0300

    building the person

--- lib/pages/account/account_page.dart
+++ lib/pages/account/account_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:playerone/widgets/app_icon.dart';
 import 'package:playerone/widgets/big_text.dart';
 
 class AccountPage extends StatelessWidget {
@@ -14,6 +15,11 @@ class AccountPage extends StatelessWidget {
           size: 24,
           color: Colors.white,
         ),
+      ),
+      body: Column(
+        children: [
+          AppIcon(icon: Icons.person,)
+        ],
       ),
     );
   }
