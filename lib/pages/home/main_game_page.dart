f014588140fe30d419432d31d4b37c871808092a
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-01 12:44:22 +0300

    refresh indicator for home page

--- lib/pages/home/main_game_page.dart
+++ lib/pages/home/main_game_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:playerone/colors.dart';
 import 'package:playerone/widgets/big_text.dart';
 import 'package:playerone/widgets/small_text.dart';
 
+import '../../data/controllers/popular_games-controller.dart';
+import '../../data/controllers/recommended_games_controller.dart';
 import 'game_page_body.dart';
 
 class MainGamePage extends StatefulWidget {
@@ -13,60 +17,64 @@ class MainGamePage extends StatefulWidget {
 }
 
 class _MainGamePage extends State<MainGamePage> {
+  Future<void> _loadResources() async {
+    await Get.find<PopularGamesController>().getPopularGamesList();
+    await Get.find<RecommendedGamesController>().getRecommendedGamesList();
+  }
+
   @override
   Widget build(BuildContext context) {
     // print("Sreen heigt is "+MediaQuery.of(context).size.height.toString());
-    return Scaffold(
-      // backgroundColor: Color(0xff3f4156),
-      body: Column(
-        children: [
-          Container(
-            child: Container(
-              margin: EdgeInsets.only(top: 49, bottom: 10),
-              padding: EdgeInsets.only(left: 20, right: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      BigText(
-                        text: "Ready, Player One",
-                        color: AppColors.mainColor,
-                      ),
-                      Row(
-                        children: [
-                          SmallText(
-                            text: "Featured Games",
-                            color: Colors.black54,
-                          ),
-                          Icon(Icons.gamepad_outlined),
-
-                        ],
-                      )
-                    ],
-                  ),
-                  Center(
-                    child: Container(
-                        width: 45,
-                        height: 45,
-                        child: Icon(
-                          Icons.search,
-                          color: Colors.white,
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
+    return RefreshIndicator(
+        child: Column(
+          children: [
+            Container(
+              child: Container(
+                margin: EdgeInsets.only(top: 49, bottom: 10),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        BigText(
+                          text: "Ready, Player One",
                           color: AppColors.mainColor,
-                        )),
-                  )
-                ],
+                        ),
+                        Row(
+                          children: [
+                            SmallText(
+                              text: "Featured Games",
+                              color: Colors.black54,
+                            ),
+                            Icon(Icons.gamepad_outlined),
+                          ],
+                        )
+                      ],
+                    ),
+                    Center(
+                      child: Container(
+                          width: 45,
+                          height: 45,
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: AppColors.mainColor,
+                          )),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(child: SingleChildScrollView(
-            child: GamePageBody(),
-          )),
-        ],
-      ),
-    );
+            Expanded(
+                child: SingleChildScrollView(
+              child: GamePageBody(),
+            )),
+          ],
+        ),
+        onRefresh: _loadResources);
   }
 }
--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -2,14 +2,14 @@ class AppConstants{
   static const String APP_NAME = "PlayerOne";
   static const double APP_VERSION = 1;
 
-  static const String BASE_URL = "https//playerone.com";
+  static const String BASE_URL = "http://127.0.0.1:8000";
 
   static const String POPULAR_GAME_URI = "/api/v1/games/popular";
   static const String RECOMMENDED_GAME_URI = "/api/v1/games/recommended";
   static const String UPLOAD_URL = "/uploads/";
 
 
-  static const String TOKEN = "PlayerOne";
+  static const String TOKEN = "playerone";
   static const String CART_LIST = "cart-list";
   static const String CART_HISTORY_LIST = "cart-history-list";
 
