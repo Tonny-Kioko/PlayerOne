87035a07cf7d4e97c97295278106f5f7d5829a0f
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-03 16:55:45 +0300

    building signup page

---
+++ lib/pages/auth/sign_up_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SignUpPae extends StatelessWidget {
+  const SignUpPae({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
