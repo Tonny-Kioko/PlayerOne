0abbc4a7eb12b0d2df4298ede62c3900d76acb44
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-03 18:40:03 +0300

    adding controllers for app text fields

--- lib/pages/auth/sign_up_page.dart
+++ lib/pages/auth/sign_up_page.dart
@@ -8,6 +8,11 @@ class SignUpPae extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var emailController = TextEditingController();
+    var confirmPasswordController = TextEditingController();
+    var passwordController = TextEditingController();
+    var nameController = TextEditingController();
+    var phoneController = TextEditingController();
+
     return Scaffold(
       backgroundColor: Colors.greenAccent,
       body: Column(
@@ -23,8 +28,46 @@ class SignUpPae extends StatelessWidget {
               ),
             ),
           ),
-          AppTextField(textController: emailController, icon: Icons.email, hintText: "Email")
-
+          //Gamer Email
+          AppTextField(
+              textController: emailController,
+              icon: Icons.email,
+              hintText: "Email"),
+          SizedBox(
+            height: Dimensions.sizeBoxHeight20,
+          ),
+          //Gamer Name
+          AppTextField(
+              textController: nameController,
+              icon: Icons.person,
+              hintText: "Gamer Name"),
+          SizedBox(
+            height: Dimensions.sizeBoxHeight20,
+          ),
+          //Mobile Number
+          AppTextField(
+              textController: phoneController,
+              icon: Icons.phone,
+              hintText: "Phone Number"),
+          SizedBox(
+            height: Dimensions.sizeBoxHeight20,
+          ),
+          //Account Password
+          AppTextField(
+              textController: passwordController,
+              icon: Icons.password,
+              hintText: "Password"),
+          SizedBox(
+            height: Dimensions.sizeBoxHeight20,
+          ),
+          //Confirm Password
+          AppTextField(
+              textController: confirmPasswordController,
+              icon: Icons.password,
+              hintText: "Confirm Password"),
+          SizedBox(
+            height: Dimensions.sizeBoxHeight20,
+          ),
         ],
       ),
     );
