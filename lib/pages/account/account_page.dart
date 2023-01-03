d616e4f2a0b45562de478827a1ecc996174bb8f6
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-03 16:53:50 +0300

    making the page scrollable

--- lib/pages/account/account_page.dart
+++ lib/pages/account/account_page.dart
@@ -36,78 +36,100 @@ class AccountPage extends StatelessWidget {
             SizedBox(
               height: Dimensions.sizeBoxHeight30,
             ),
-            //Gaming name
-            AccountWidget(
-                bigText: BigText(
-                  text: "Mickey",
-                ),
-                appIcon: AppIcon(
-                  icon: Icons.person,
-                  backgroundColor: AppColors.mainColor,
-                  iconColor: Colors.white,
-                  iconSize: Dimensions.sizeBoxHeight30,
-                  size: Dimensions.sizeBoxHeight50,
-                )),
-            SizedBox(
-              height: Dimensions.sizeBoxHeight30,
-            ),
-            //Mobile Number
-            AccountWidget(
-                bigText: BigText(
-                  text: "+25471515",
-                ),
-                appIcon: AppIcon(
-                  icon: Icons.phone,
-                  backgroundColor: Colors.blue,
-                  iconColor: Colors.white,
-                  iconSize: Dimensions.sizeBoxHeight30,
-                  size: Dimensions.sizeBoxHeight50,
-                )),
-            SizedBox(
-              height: Dimensions.sizeBoxHeight30,
-            ),
-            //Email
-            AccountWidget(
-                bigText: BigText(
-                  text: "tonny@email.com",
-                ),
-                appIcon: AppIcon(
-                  icon: Icons.email,
-                  backgroundColor: Colors.blue,
-                  iconColor: Colors.white,
-                  iconSize: Dimensions.sizeBoxHeight30,
-                  size: Dimensions.sizeBoxHeight50,
-                )),
-            SizedBox(
-              height: Dimensions.sizeBoxHeight30,
-            ),
-            //Delivery Address
-            AccountWidget(
-                bigText: BigText(
-                  text: "Nairobi",
-                ),
-                appIcon: AppIcon(
-                  icon: Icons.location_on,
-                  backgroundColor: Colors.yellow,
-                  iconColor: Colors.white,
-                  iconSize: Dimensions.sizeBoxHeight30,
-                  size: Dimensions.sizeBoxHeight50,
-                )),
-            SizedBox(
-              height: Dimensions.sizeBoxHeight30,
-            ),
-            //Important notifications
-            AccountWidget(
-                bigText: BigText(
-                  text: "Check what's happenin...",
-                ),
-                appIcon: AppIcon(
-                  icon: Icons.notifications_active_sharp,
-                  backgroundColor: Colors.redAccent,
-                  iconColor: Colors.white,
-                  iconSize: Dimensions.sizeBoxHeight30,
-                  size: Dimensions.sizeBoxHeight50,
-                )),
+            Expanded(
+                child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  //Gaming name
+                  AccountWidget(
+                      bigText: BigText(
+                        text: "Mickey",
+                      ),
+                      appIcon: AppIcon(
+                        icon: Icons.person,
+                        backgroundColor: AppColors.mainColor,
+                        iconColor: Colors.white,
+                        iconSize: Dimensions.sizeBoxHeight30,
+                        size: Dimensions.sizeBoxHeight50,
+                      )),
+                  SizedBox(
+                    height: Dimensions.sizeBoxHeight30,
+                  ),
+                  //Mobile Number
+                  AccountWidget(
+                      bigText: BigText(
+                        text: "+25471515",
+                      ),
+                      appIcon: AppIcon(
+                        icon: Icons.phone,
+                        backgroundColor: Colors.blue,
+                        iconColor: Colors.white,
+                        iconSize: Dimensions.sizeBoxHeight30,
+                        size: Dimensions.sizeBoxHeight50,
+                      )),
+                  SizedBox(
+                    height: Dimensions.sizeBoxHeight30,
+                  ),
+                  //Email
+                  AccountWidget(
+                      bigText: BigText(
+                        text: "tonny@email.com",
+                      ),
+                      appIcon: AppIcon(
+                        icon: Icons.email,
+                        backgroundColor: Colors.blue,
+                        iconColor: Colors.white,
+                        iconSize: Dimensions.sizeBoxHeight30,
+                        size: Dimensions.sizeBoxHeight50,
+                      )),
+                  SizedBox(
+                    height: Dimensions.sizeBoxHeight30,
+                  ),
+                  //Delivery Address
+                  AccountWidget(
+                      bigText: BigText(
+                        text: "Nairobi",
+                      ),
+                      appIcon: AppIcon(
+                        icon: Icons.location_on,
+                        backgroundColor: Colors.yellow,
+                        iconColor: Colors.white,
+                        iconSize: Dimensions.sizeBoxHeight30,
+                        size: Dimensions.sizeBoxHeight50,
+                      )),
+                  SizedBox(
+                    height: Dimensions.sizeBoxHeight30,
+                  ),
+                  //Important notifications
+                  AccountWidget(
+                      bigText: BigText(
+                        text: "Check what's happenin...",
+                      ),
+                      appIcon: AppIcon(
+                        icon: Icons.notifications_active_sharp,
+                        backgroundColor: Colors.redAccent,
+                        iconColor: Colors.white,
+                        iconSize: Dimensions.sizeBoxHeight30,
+                        size: Dimensions.sizeBoxHeight50,
+                      )),
+                  SizedBox(
+                    height: Dimensions.sizeBoxHeight30,
+                  ),
+                  //Payment Information
+                  AccountWidget(
+                      bigText: BigText(
+                        text: "Payment Details",
+                      ),
+                      appIcon: AppIcon(
+                        icon: Icons.payments,
+                        backgroundColor: Colors.green,
+                        iconColor: Colors.white,
+                        iconSize: Dimensions.sizeBoxHeight30,
+                        size: Dimensions.sizeBoxHeight50,
+                      )),
+                ],
+              ),
+            ))
           ],
         ),
       ),
