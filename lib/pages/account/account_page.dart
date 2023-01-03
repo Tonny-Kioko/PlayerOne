d1a2168478c4ead753d8527ef0e7ddbf6903a3c3
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-03 16:48:48 +0300

    setting up profile information

--- lib/pages/account/account_page.dart
+++ lib/pages/account/account_page.dart
@@ -21,7 +21,6 @@ class AccountPage extends StatelessWidget {
         ),
       ),
       body: Container(
-
         width: double.maxFinite,
         margin: EdgeInsets.only(top: Dimensions.sizeBoxHeight10),
         child: Column(
@@ -32,45 +31,79 @@ class AccountPage extends StatelessWidget {
               backgroundColor: AppColors.mainColor,
               iconColor: Colors.white,
               iconSize: Dimensions.sizeBoxHeight50 + Dimensions.sizeBoxHeight20,
-              size: Dimensions.sizeBoxHeight50*3,
+              size: Dimensions.sizeBoxHeight50 * 3,
             ),
-            SizedBox(height: Dimensions.sizeBoxHeight30,),
+            SizedBox(
+              height: Dimensions.sizeBoxHeight30,
+            ),
             //Gaming name
-            AccountWidget(bigText: BigText(text: "pal",),
+            AccountWidget(
+                bigText: BigText(
+                  text: "Mickey",
+                ),
                 appIcon: AppIcon(
-              icon: Icons.person,
-              backgroundColor: AppColors.mainColor,
-              iconColor: Colors.white,
-              iconSize: Dimensions.sizeBoxHeight30,
+                  icon: Icons.person,
+                  backgroundColor: AppColors.mainColor,
+                  iconColor: Colors.white,
+                  iconSize: Dimensions.sizeBoxHeight30,
                   size: Dimensions.sizeBoxHeight50,
-            )),
-            SizedBox(height: Dimensions.sizeBoxHeight30,),
+                )),
+            SizedBox(
+              height: Dimensions.sizeBoxHeight30,
+            ),
             //Mobile Number
-            AccountWidget(bigText: BigText(text: "pal",),
+            AccountWidget(
+                bigText: BigText(
+                  text: "+25471515",
+                ),
                 appIcon: AppIcon(
-                  icon: Icons.person,
-                  backgroundColor: AppColors.mainColor,
+                  icon: Icons.phone,
+                  backgroundColor: Colors.blue,
                   iconColor: Colors.white,
                   iconSize: Dimensions.sizeBoxHeight30,
                   size: Dimensions.sizeBoxHeight50,
                 )),
-            SizedBox(height: Dimensions.sizeBoxHeight30,),
+            SizedBox(
+              height: Dimensions.sizeBoxHeight30,
+            ),
             //Email
-            AccountWidget(bigText: BigText(text: "pal",),
+            AccountWidget(
+                bigText: BigText(
+                  text: "tonny@email.com",
+                ),
                 appIcon: AppIcon(
-                  icon: Icons.person,
-                  backgroundColor: AppColors.mainColor,
+                  icon: Icons.email,
+                  backgroundColor: Colors.blue,
                   iconColor: Colors.white,
                   iconSize: Dimensions.sizeBoxHeight30,
                   size: Dimensions.sizeBoxHeight50,
                 )),
+            SizedBox(
+              height: Dimensions.sizeBoxHeight30,
+            ),
             //Delivery Address
-            SizedBox(height: Dimensions.sizeBoxHeight30,),
-            //Important notifications
-            AccountWidget(bigText: BigText(text: "pal",),
+            AccountWidget(
+                bigText: BigText(
+                  text: "Nairobi",
+                ),
                 appIcon: AppIcon(
-                  icon: Icons.person,
-                  backgroundColor: AppColors.mainColor,
+                  icon: Icons.location_on,
+                  backgroundColor: Colors.yellow,
+                  iconColor: Colors.white,
+                  iconSize: Dimensions.sizeBoxHeight30,
+                  size: Dimensions.sizeBoxHeight50,
+                )),
+            SizedBox(
+              height: Dimensions.sizeBoxHeight30,
+            ),
+            //Important notifications
+            AccountWidget(
+                bigText: BigText(
+                  text: "Check what's happenin...",
+                ),
+                appIcon: AppIcon(
+                  icon: Icons.notifications_active_sharp,
+                  backgroundColor: Colors.redAccent,
                   iconColor: Colors.white,
                   iconSize: Dimensions.sizeBoxHeight30,
                   size: Dimensions.sizeBoxHeight50,
