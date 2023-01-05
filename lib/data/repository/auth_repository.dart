b579586a1e92471520e3bc250ee3b26284e03f69
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-05 11:49:39 +0300

    setting up auth repo

---
+++ lib/data/repository/auth_repository.dart
@@ -0,0 +1,9 @@
+import 'package:playerone/data/api/api_client.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthRepo {
+  final ApiClient apiClient;
+  final SharedPreferences sharedPreferences;
+
+  AuthRepo({required this.apiClient, required this.sharedPreferences});
+}
