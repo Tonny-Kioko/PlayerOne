d415c74b458d5d6b254238c38d395a8809658398
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-16 15:29:31 +0300

    setting up user controller

---
+++ lib/data/controllers/user_controller.dart
@@ -0,0 +1,35 @@
+import 'package:get/get_connect/http/src/response/response.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_disposable.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:playerone/data/repository/user_repository.dart';
+import 'package:playerone/models/user_model.dart';
+
+import '../../models/response_models.dart';
+
+class UserController extends GetxController implements GetxService {
+  final UserRepo userRepo;
+
+  UserController({required this.userRepo});
+
+  bool _isLoading = false;
+  late UserModel _userModel;
+
+  bool get isLoading => _isLoading;
+  UserModel get userModel => userModel;
+
+  Future<ResponseModel> getUserInfo() async {
+    _isLoading = true;
+    update();
+    Response response = await userRepo.getUserInfo();
+    late ResponseModel responseModel;
+    if (response.statusCode == 200) {
+      _userModel = UserModel.fromJson(response.body);
+      responseModel = ResponseModel(true, "I worked well");
+    } else {
+      responseModel = ResponseModel(false, response.statusText!);
+    }
+    _isLoading = false;
+    update();
+    return responseModel;
+  }
+}
